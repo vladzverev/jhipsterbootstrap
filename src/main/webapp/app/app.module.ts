6695c621e4e56d403009f6859b2d96459dacc461
Vlad <vlad_imir_07@mail.ru>
2017-03-29 23:45:13 +0300

    Initial commit

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,57 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { JhipsterSharedModule, UserRouteAccessService } from './shared';
+import { JhipsterHomeModule } from './home/home.module';
+import { JhipsterAdminModule } from './admin/admin.module';
+import { JhipsterAccountModule } from './account/account.module';
+import { JhipsterEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        JhipsterSharedModule,
+        JhipsterHomeModule,
+        JhipsterAdminModule,
+        JhipsterAccountModule,
+        JhipsterEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class JhipsterAppModule {}
---
+++ src/main/webapp/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { JhipsterSharedModule } from '../shared';
+
+import { HOME_ROUTE, HomeComponent } from './';
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+    imports: [
+        MaterialModule,
+        JhipsterSharedModule,
+        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true })
+    ],
+    declarations: [
+        HomeComponent,
+    ],
+    entryComponents: [
+    ],
+    providers: [
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class JhipsterHomeModule {}
